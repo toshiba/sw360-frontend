370e08e0eedb4a4cd7375929f355039d367f8e7f
tuannn2 <tuan2.nguyennhu@toshiba.co.jp>
2023-07-13 17:01:46 +0700

    feat(component): Add tab releases in Edit component
    Signed-off-by: tuannn2 <tuan2.nguyennhu@toshiba.co.jp>

---
+++ src/object-types/enums/AttachmentTypes.ts
@@ -0,0 +1,35 @@
+// Copyright (C) TOSHIBA CORPORATION, 2023. Part of the SW360 Frontend Project.
+// Copyright (C) Toshiba Software Development (Vietnam) Co., Ltd., 2023. Part of the SW360 Frontend Project.
+
+// This program and the accompanying materials are made
+// available under the terms of the Eclipse Public License 2.0
+// which is available at https://www.eclipse.org/legal/epl-2.0/
+
+// SPDX-License-Identifier: EPL-2.0
+// License-Filename: LICENSE
+
+enum AttachmentTypes {
+    SOURCE = 'Source File',
+    DESIGN = 'Design',
+    REQUIREMENT = 'Requirement',
+    CLEARING_REPORT = 'Clearing Report',
+    COMPONENT_LICENSE_INFO_XML = 'Component License Info XML',
+    COMPONENT_LICENSE_INFO_COMBINED = 'Component License Info XML Combined',
+    SCAN_RESULT_REPORT = 'Scan Result Report',
+    SCAN_RESULT_REPORT_XML = 'Scan Result Report XML',
+    SOURCE_SELF = 'Source Self',
+    BINARY = 'Binary',
+    BINARY_SELF = 'Binary Self',
+    DECISION_REPORT = 'Decision Report',
+    LEGAL_EVALUATION = 'Legal Evaluation',
+    LICENSE_AGREEMENT = 'License Agreement',
+    SCREENSHOT = 'Screenshot',
+    OTHER = 'other',
+    README_OSS = 'Readme OSS',
+    SECURITY_ASSESSMENT = 'Secirity Assessment',
+    INITIAL_SCAN_REPORT = 'Initial Scan Report',
+    SBOM = 'SBOM',
+    INTERNAL_USE_SCAN = 'Internal Use Scan',
+}
+
+export default AttachmentTypes
