31e1ad770bf874418310de1bb8ad6d821e2a95fa
tuannn2 <tuan2.nguyennhu@toshiba.co.jp>
2023-08-10 10:03:13 +0700

    feat(release): Feature add release summary UI
    Signed-off-by: tuannn2 <tuan2.nguyennhu@toshiba.co.jp>

---
+++ src/app/[locale]/components/releases/add/releases/AddRelease.tsx
@@ -0,0 +1,70 @@
+// Copyright (C) TOSHIBA CORPORATION, 2023. Part of the SW360 Frontend Project.
+// Copyright (C) Toshiba Software Development (Vietnam) Co., Ltd., 2023. Part of the SW360 Frontend Project.
+
+// This program and the accompanying materials are made
+// available under the terms of the Eclipse Public License 2.0
+// which is available at https://www.eclipse.org/legal/epl-2.0/
+
+// SPDX-License-Identifier: EPL-2.0
+// License-Filename: LICENSE
+
+'use client'
+import { useState } from 'react'
+
+import CommonTabIds from '@/object-types/enums/CommonTabsIds'
+import { Session } from '@/object-types/Session'
+import { useTranslations } from 'next-intl'
+import { COMMON_NAMESPACE } from '@/object-types/Constants'
+import { SideBar, PageButtonHeader } from '@/components/sw360'
+import ReleaseTabIds from '@/object-types/enums/ReleaseTabIds'
+import ReleaseAddSummary from '@/components/releases/ReleaseAddSummary'
+import LinkedReleases from '@/components/releases/LinkedReleases/LinkedReleases'
+
+interface Props {
+    session?: Session
+    componentId?: string
+}
+
+const tabList = [
+    {
+        id: CommonTabIds.SUMMARY,
+        name: 'Summary',
+    },
+    {
+        id: ReleaseTabIds.LINKED_RELEASES,
+        name: 'Linked Releases',
+    },
+]
+
+const AddRelease = ({ session, componentId }: Props) => {
+    const [selectedTab, setSelectedTab] = useState<string>(CommonTabIds.SUMMARY)
+    const headerButtons = {
+        'Create Release': { link: '/components/' + componentId, type: 'primary' },
+        Cancel: { link: '/components/detail/' + componentId, type: 'secondary' },
+    }
+
+    return (
+        <>
+            <div className='container' style={{ maxWidth: '98vw', marginTop: '10px' }}>
+                <div className='row'>
+                    <div className='col-2 sidebar'>
+                        <SideBar selectedTab={selectedTab} setSelectedTab={setSelectedTab} tabList={tabList} />
+                    </div>
+                    <div className='col'>
+                        <div className='row' style={{ marginBottom: '20px' }}>
+                            <PageButtonHeader buttons={headerButtons}></PageButtonHeader>
+                        </div>
+                        <div className='row' hidden={selectedTab !== CommonTabIds.SUMMARY ? true : false}>
+                            <ReleaseAddSummary session={session} />
+                        </div>
+                        <div className='row' hidden={selectedTab != ReleaseTabIds.LINKED_RELEASES ? true : false}>
+                            <LinkedReleases />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default AddRelease
